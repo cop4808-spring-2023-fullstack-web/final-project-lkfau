a6259716c43acc7ade88131100fd11b6f3b0e233
logankarstens <lkarstens2021@fau.edu>
2023-04-29 18:59:27 -0400

    fix details

--- frontend/src/Components/Content/Details/Details.js
+++ frontend/src/Components/Content/Details/Details.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
-import styles from "./Details.module.css";
+/*import styles from "./Details.module.css";*/
 import Rating from "../../UI/Rating/Rating";
 import ImageCarousel from "./ImageCarousel/ImagesCarousel";
 import Reviews from "./Reviews/Reviews";
@@ -8,36 +8,33 @@ import Favorite from "./FavoriteCheckbox/FavoriteCheckbox";
 import Card from "../../UI/Card/Card";
 const Details = ({ restaurant, reviews }) => {
   return (
-    
     <Card className="p-5">
       <Container>
-      
-      <Row>
-        <Col className="d-flex flex-column" xs={12} lg={6}>
-          <h1 className="display-2">{restaurant.name}</h1>
-          <Rating rating={restaurant.rating} />
-          <Favorite restaurant_name={restaurant.name} business_id={restaurant.id}/>
-        </Col>
-        <Col xs={12} lg={6} className="d-flex justify-content-center"></Col>
-      </Row>
-      <Row className="pb-4">
-        <Col>
-        <h3>Pictures</h3>
-        <ImageCarousel photos={restaurant.photos}/>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-        <h3>Reviews ({restaurant.review_count})</h3>
-        </Col>
-        {reviews &&
-        <Reviews reviews={reviews.reviews}></Reviews>
-}
-      </Row>
+        <Row>
+          <Col className="d-flex flex-column" xs={12} lg={6}>
+            <h1 className="display-2">{restaurant.name}</h1>
+            <Rating rating={restaurant.rating} />
+            <Favorite
+              restaurant_name={restaurant.name}
+              business_id={restaurant.id}
+            />
+          </Col>
+          <Col xs={12} lg={6} className="d-flex justify-content-center"></Col>
+        </Row>
+        <Row className="pb-4">
+          <Col>
+            <h3>Pictures</h3>
+            <ImageCarousel photos={restaurant.photos} />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h3>Reviews ({restaurant.review_count})</h3>
+          </Col>
+          {reviews && <Reviews reviews={reviews.reviews}></Reviews>}
+        </Row>
       </Container>
     </Card>
-  
-  
   );
 };
 
