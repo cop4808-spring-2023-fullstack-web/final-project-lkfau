8052ef4a5d5b5160a8315cf81a66c573aa732602
logankarstens <lkarstens2021@fau.edu>
2023-05-01 19:50:04 -0400

    fix notfound

--- frontend/src/Components/Pages/NotFound/NotFound.js
+++ frontend/src/Components/Pages/NotFound/NotFound.js
@@ -1,4 +1,11 @@
+import { Container, Alert } from "react-bootstrap";
 const NotFound = () => {
-  return <h1>NotFound</h1>;
+  return (
+    <Container className="pt-5">
+      <Alert variant="danger">
+        <h2>404 - Page not found</h2>
+      </Alert>
+    </Container>
+  );
 };
 export default NotFound;
